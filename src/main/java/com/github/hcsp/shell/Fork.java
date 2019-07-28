f87578c2528b1cbb678c8cb1f0f43eded6853294
XiongAmao <onycat@live.cn>
2019-07-28 23:48:53 +0800

    Java fork process.

--- src/main/java/com/github/hcsp/shell/Fork.java
+++ src/main/java/com/github/hcsp/shell/Fork.java
@@ -3,6 +3,7 @@ package com.github.hcsp.shell;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
 
 public class Fork {
     public static void main(String[] args) throws Exception {
@@ -10,6 +11,17 @@ public class Fork {
         // 工作目录是项目目录下的working-directory目录（可以用getWorkingDir()方法得到这个目录对应的File对象）
         // 传递的命令是sh run.sh
         // 环境变量是AAA=123
+
+        // (bin , params)
+        ProcessBuilder pb = new ProcessBuilder("sh", "run.sh");
+        // working dir path
+        pb.directory(getWorkingDir());
+        // env
+        Map<String, String> envs = pb.environment();
+        envs.put("AAA", "123");
+
+        pb.redirectOutput(getOutputFile());
+        pb.start().waitFor();
     }
 
     private static File getWorkingDir() {
