55463bc676bf8e71bbee607e3dcd59719833b9ff
Ben <50296011+MaoYuanFang@users.noreply.github.com>
2019-07-29 10:33:29 +0800

    Update Fork.java

--- src/main/java/com/github/hcsp/shell/Fork.java
+++ src/main/java/com/github/hcsp/shell/Fork.java
@@ -11,8 +11,8 @@ public class Fork {
         // 工作目录是项目目录下的working-directory目录（可以用getWorkingDir()方法得到这个目录对应的File对象）
         // 传递的命令是sh run.sh
         // 环境变量是AAA=123
-        ProcessBuilder pd = new ProcessBuilder("sh","run.sh");
-        pd.directory(getWorkingDir());
+        ProcessBuilder pb = new ProcessBuilder("sh","run.sh");
+        pb.directory(getWorkingDir());
         Map<String,String> envs = pb.enviroment();
         envs.put("AAA","123");
         pb.start().waitFor();
