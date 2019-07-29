7f0715a60d4f083075f17628022cf68e790461bb
dingsheng <dingduoduo0916@gmail.com>
2019-07-29 15:56:36 +0800

    ProcessBuilder fork子进程

--- README.md
+++ README.md
@@ -15,3 +15,4 @@
 完成题目有困难？不妨来看看[写代码啦的相应课程](https://xiedaimala.com/tasks/661cd7ab-7fea-47d0-8e11-555d6fca751d)吧！
 
 回到[写代码啦的题目](https://xiedaimala.com/tasks/661cd7ab-7fea-47d0-8e11-555d6fca751d/quizzes/6c87ef57-7f06-4af2-9112-86dd27ff099d)，继续挑战！
+
--- src/main/java/com/github/hcsp/shell/Fork.java
+++ src/main/java/com/github/hcsp/shell/Fork.java
@@ -3,21 +3,28 @@ package com.github.hcsp.shell;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Fork {
-    public static void main(String[] args) throws Exception {
-        // 请在这里使用Java代码fork一个子进程，将fork的子进程的标准输出重定向到指定文件：工作目录下名为output.txt的文件
-        // 工作目录是项目目录下的working-directory目录（可以用getWorkingDir()方法得到这个目录对应的File对象）
-        // 传递的命令是sh run.sh
-        // 环境变量是AAA=123
-    }
+  public static void main(String[] args) throws Exception {
+    // 请在这里使用Java代码fork一个子进程，将fork的子进程的标准输出重定向到指定文件：工作目录下名为output.txt的文件
+    // 工作目录是项目目录下的working-directory目录（可以用getWorkingDir()方法得到这个目录对应的File对象）
+    // 传递的命令是sh run.sh
+    // 环境变量是AAA=123
+    ProcessBuilder pb = new ProcessBuilder("sh", "run.sh");
+    pb.directory(getWorkingDir())
+            .redirectOutput(getOutputFile())
+            .environment().put("AAA", "123");
+    pb.start();
+  }
 
-    private static File getWorkingDir() {
-        Path projectDir = Paths.get(System.getProperty("user.dir"));
-        return projectDir.resolve("working-directory").toFile();
-    }
+  private static File getWorkingDir() {
+    Path projectDir = Paths.get(System.getProperty("user.dir"));
+    return projectDir.resolve("working-directory").toFile();
+  }
 
-    private static File getOutputFile() {
-        return new File(getWorkingDir(), "output.txt");
-    }
+  private static File getOutputFile() {
+    return new File(getWorkingDir(), "output.txt");
+  }
 }
