23f11e8a59b0cc8f851d209ba4e9a9fc92531d99
Stanley <phlvance1208@foxmail.com>
2019-10-16 02:18:24 +0800

    fork子进程

--- src/main/java/com/github/hcsp/shell/Fork.java
+++ src/main/java/com/github/hcsp/shell/Fork.java
@@ -3,13 +3,24 @@ package com.github.hcsp.shell;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
 
 public class Fork {
     public static void main(String[] args) throws Exception {
         // 请在这里使用Java代码fork一个子进程，将fork的子进程的标准输出重定向到指定文件：工作目录下名为output.txt的文件
         // 工作目录是项目目录下的working-directory目录（可以用getWorkingDir()方法得到这个目录对应的File对象）
-        // 传递的命令是sh run.sh
+        // 传递的命令/参数是sh run.sh
         // 环境变量是AAA=123
+
+        //1.可执行程序 2.参数
+        ProcessBuilder processBuilder = new ProcessBuilder("sh","run.sh");
+        //3.工作目录
+        processBuilder.directory(getWorkingDir());
+        //4.环境变量
+        Map<String,String> envs = processBuilder.environment();
+        envs.put("AAA","123");
+        //5.启动processBuilder
+        processBuilder.start().waitFor();
     }
 
     private static File getWorkingDir() {
