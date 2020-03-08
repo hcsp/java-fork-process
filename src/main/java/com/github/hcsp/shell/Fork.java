8dc1a7ca76759a68f488ef40cec18ea764909e93
Leo Feng <553931681@qq.com>
2020-03-08 20:16:39 +0800

    使用JAVA代码fork子进程 (#114)
    使用JAVA代码fork子进程 (#114)

--- src/main/java/com/github/hcsp/shell/Fork.java
+++ src/main/java/com/github/hcsp/shell/Fork.java
@@ -3,6 +3,7 @@ package com.github.hcsp.shell;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
 
 public class Fork {
     public static void main(String[] args) throws Exception {
@@ -10,6 +11,20 @@ public class Fork {
         // 工作目录是项目目录下的working-directory目录（可以用getWorkingDir()方法得到这个目录对应的File对象）
         // 传递的命令是sh run.sh
         // 环境变量是AAA=123
+
+        // 创建ProcessBuilder对象
+        ProcessBuilder processBuilder = new ProcessBuilder("sh", "run.sh");
+        // 指定子进程的工作目录
+        processBuilder.directory(getWorkingDir());
+        // 获取父进程的环境变量
+        Map<String, String> envs = processBuilder.environment();
+        // 添加新的环境变量
+        envs.put("AAA", "123");
+        // 将输出重定向，如果将此进程中启动的子进程的输出在此父进程中输出出来，应使用
+        // processBuilder.inheritIO();
+        processBuilder.redirectOutput(getOutputFile());
+        // 启动子进程，并阻塞父进程，直到子进程退出
+        processBuilder.start().waitFor();
     }
 
     private static File getWorkingDir() {
