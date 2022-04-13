a35ce001079570ea0169e2b3d9f9aab27b5b73ed
lx <iamxliu@163.com>
2022-04-13 18:22:45 +0800

    1

--- src/main/java/com/github/hcsp/shell/Fork.java
+++ src/main/java/com/github/hcsp/shell/Fork.java
@@ -16,6 +16,7 @@ public class Fork {
         Map<String, String> m = pb.environment();
         m.put("AAA", "123");
         pb.redirectOutput(getOutputFile());
+
         pb.start().waitFor();
     }
 
