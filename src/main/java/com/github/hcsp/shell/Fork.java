9f71a23aee59ef70da54204414ed992ce49edf4f
bycgxss <bycgxss@gmail.com>
2022-04-19 15:16:21 +0800

    java fork 子进程

--- src/main/java/com/github/hcsp/shell/Fork.java
+++ src/main/java/com/github/hcsp/shell/Fork.java
@@ -13,8 +13,7 @@ public class Fork {
         // 环境变量是AAA=123
         ProcessBuilder pb = new ProcessBuilder("sh", "run.sh");
         pb.directory(getWorkingDir());
-        Map<String, String> envs = pb.environment();
-        envs.put("AAA", "123");
+        pb.environment().put("AAA", "123");
         pb.redirectOutput(getOutputFile());
         pb.start().waitFor();
     }
